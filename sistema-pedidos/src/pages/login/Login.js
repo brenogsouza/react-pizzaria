9fab461208cae2b9ed376b015199d824bbeade2e
brenogsouza <breno.souza.developer@gmail.com>
2020-03-23 18:07:15 -0300

    iniciado tela de login

--- sistema-pedidos/src/pages/login/Login.js
+++ sistema-pedidos/src/pages/login/Login.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import {
+  Button, Grid, Divider, Typography, TextField, Checkbox,
+} from '@material-ui/core';
 import { Logo } from '../../images';
 
 // import { Container } from './styles';
 
 const Login = () => (
-  <div>
-    <img src={Logo} alt="Logo" />
-    <Button>Entrar com github</Button>
-  </div>
+  <Grid container spacing={6}>
+    <Grid item xs={12}>
+      <img src={Logo} alt="Logo" />
+    </Grid>
+    <Grid item xs={12}>
+      <Button>Entrar com github</Button>
+    </Grid>
+    <Divider orientation="horizontal" />
+    <Grid item xs={12}>
+      <Typography>ou</Typography>
+    </Grid>
+    <Divider orientation="horizontal" />
+    <Grid item xs={12}>
+      <TextField variant="outlined" label="E-mail" fullWidth />
+    </Grid>
+    <Grid item xs={12}>
+      <TextField variant="outlined" label="Senha" fullWidth />
+    </Grid>
+    <Grid item xs={6}>
+      <Checkbox />
+    </Grid>
+    <Grid item xs={6}>
+      <Button>Entrar</Button>
+    </Grid>
+  </Grid>
 );
 
 export default Login;
