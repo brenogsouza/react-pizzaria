945fc88a86f8262724a2aa6ee8ec567afda36416
brenogsouza <breno.souza.developer@gmail.com>
2020-03-23 01:57:59 -0300

    iniciando as rotas internas

--- sistema-pedidos/src/configs/routes.js
+++ sistema-pedidos/src/configs/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Login } from '../pages/login';
+import { Home } from '../pages/home';
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" />
     <Route path="/login" component={Login} />
+    <Route exact path="/" component={Home} />
   </Switch>
 );
 
