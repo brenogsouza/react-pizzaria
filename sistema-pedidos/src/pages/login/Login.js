c856c4c92d4e1974023ccb55aa184f22906e6dc4
brenogsouza <breno.souza.developer@gmail.com>
2020-03-23 20:46:27 -0300

    adicionado styles e setado configurações do Grid

--- sistema-pedidos/src/pages/login/Login.js
+++ sistema-pedidos/src/pages/login/Login.js
@@ -4,34 +4,38 @@ import {
 } from '@material-ui/core';
 import { Logo } from '../../images';
 
+import { Container } from './styles';
+
 // import { Container } from './styles';
 
 const Login = () => (
-  <Grid container spacing={6}>
-    <Grid item xs={12}>
-      <img src={Logo} alt="Logo" />
+  <Container>
+    <Grid container spacing={10} direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <img src={Logo} alt="Logo" />
+      </Grid>
+      <Grid item xs={12}>
+        <Button>Entrar com github</Button>
+      </Grid>
+      <Divider orientation="horizontal" />
+      <Grid item xs={12}>
+        <Typography>ou</Typography>
+      </Grid>
+      <Divider orientation="horizontal" />
+      <Grid item xs={12}>
+        <TextField variant="outlined" label="E-mail" fullWidth />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField variant="outlined" label="Senha" fullWidth />
+      </Grid>
+      <Grid item xs={6}>
+        <Checkbox />
+      </Grid>
+      <Grid item xs={6}>
+        <Button>Entrar</Button>
+      </Grid>
     </Grid>
-    <Grid item xs={12}>
-      <Button>Entrar com github</Button>
-    </Grid>
-    <Divider orientation="horizontal" />
-    <Grid item xs={12}>
-      <Typography>ou</Typography>
-    </Grid>
-    <Divider orientation="horizontal" />
-    <Grid item xs={12}>
-      <TextField variant="outlined" label="E-mail" fullWidth />
-    </Grid>
-    <Grid item xs={12}>
-      <TextField variant="outlined" label="Senha" fullWidth />
-    </Grid>
-    <Grid item xs={6}>
-      <Checkbox />
-    </Grid>
-    <Grid item xs={6}>
-      <Button>Entrar</Button>
-    </Grid>
-  </Grid>
+  </Container>
 );
 
 export default Login;
