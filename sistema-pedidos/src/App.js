2db89a0367520bbd642d25ffdce1452815ffcf8f
brenogsouza <breno.souza.developer@gmail.com>
2020-03-22 21:39:48 -0300

    setado rescript para pegar setar linter criado pelo CRA

--- sistema-pedidos/src/App.js
+++ sistema-pedidos/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-
+import React from 'react'
 
 function App() {
   return (
     <div>
       <h1>ola mundo</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
